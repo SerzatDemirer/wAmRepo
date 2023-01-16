8da2c87e56be533a0083eede48c0c1463b9a3710
Serzat Demirer <serzat.demirer1@gmail.com>
2023-01-16 11:09:00 +0100

    Initialize commit: Created projeckt, moleboard component and game service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MoleboardComponent } from './moleboard/moleboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoleboardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/moleboard/moleboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-moleboard',
+  templateUrl: './moleboard.component.html',
+  styleUrls: ['./moleboard.component.css']
+})
+export class MoleboardComponent {
+
+}
